4daaac9adb8a742f320815868453460e95c26e72
John Mason <johnmsn22@gmail.com>
2024-11-01 22:47:38 +0100

    shop section ui layout

--- src/components/home/ShopSection.tsx
+++ src/components/home/ShopSection.tsx
@@ -6,7 +6,7 @@ const ShopSection = () => {
   return (
     <section className="pt-[50px]">
       <div className="mx-auto w-[95%]">
-        <h2 className=" text-[160px] lowercase font-bold text-white -tracking-wider leading-none mx-auto w-[90%] translate-y-10 z-[1] relative">
+        <h2 className=" text-[160px] lowercase font-bold text-white -tracking-wider leading-none mx-auto w-[90%] translate-y-10 z-[2] relative">
           Shop
         </h2>
 
@@ -21,7 +21,111 @@ const ShopSection = () => {
             </button>
           </div>
 
-          <div className="relative overflow-hidden"></div>
+          <div className="relative overflow-hidden">
+            <div className="w-full relative h-full">
+              <article className="article__card">
+                <figure className="relative block">
+                  <div className="article_card__thumb pb-[100%] relative">
+                    <div className="article_card__thumb-overlay">
+                      <Link href="#">
+                        <Button variant="outline">shop now</Button>
+                      </Link>
+                    </div>
+                    <div className="__thumb-bg-color"></div>
+                    <div className="__thumb-bg-image"></div>
+                  </div>
+                  <div className="article_card__footer">
+                    <div className="__footer-header">
+                      <h3>Wicked: The Soundtrack (Picture Disc Vinyl)</h3>
+                      <h4>$51.00</h4>
+                    </div>
+                  </div>
+                </figure>
+              </article>
+              <article className="article__card">
+                <figure className="relative block">
+                  <div className="article_card__thumb pb-[100%] relative">
+                    <div className="article_card__thumb-overlay">
+                      <Link href="#">
+                        <Button variant="outline">shop now</Button>
+                      </Link>
+                    </div>
+                    <div className="__thumb-bg-color"></div>
+                    <div className="__thumb-bg-image"></div>
+                  </div>
+                  <div className="article_card__footer">
+                    <div className="__footer-header">
+                      <h3>Wicked: The Soundtrack (Picture Disc Vinyl)</h3>
+                      <h4>$51.00</h4>
+                    </div>
+                  </div>
+                </figure>
+              </article>
+              <article className="article__card">
+                <figure className="relative block">
+                  <div className="article_card__thumb pb-[100%] relative">
+                    <div className="article_card__thumb-overlay">
+                      <Link href="#">
+                        <Button variant="outline">shop now</Button>
+                      </Link>
+                    </div>
+                    <div className="__thumb-bg-color"></div>
+                    <div className="__thumb-bg-image"></div>
+                  </div>
+                  <div className="article_card__footer">
+                    <div className="__footer-header">
+                      <h3>Wicked: The Soundtrack (Picture Disc Vinyl)</h3>
+                      <h4>$51.00</h4>
+                    </div>
+                  </div>
+                </figure>
+              </article>
+              <article className="article__card">
+                <figure className="relative block">
+                  <div className="article_card__thumb pb-[100%] relative">
+                    <div className="article_card__thumb-overlay">
+                      <Link href="#">
+                        <Button variant="outline">shop now</Button>
+                      </Link>
+                    </div>
+                    <div className="__thumb-bg-color"></div>
+                    <div
+                      className="__thumb-bg-image"
+                      // style={{
+                      //   backgroundImage:
+                      //     "url(https://www.republicrecords.com/files/2024/10/wicked-picturediscvinyl.png)",
+                      // }}
+                    ></div>
+                  </div>
+                  <div className="article_card__footer">
+                    <div className="__footer-header">
+                      <h3>Wicked: The Soundtrack (Picture Disc Vinyl)</h3>
+                      <h4>$51.00</h4>
+                    </div>
+                  </div>
+                </figure>
+              </article>
+              <article className="article__card">
+                <figure className="relative block">
+                  <div className="article_card__thumb pb-[100%] relative">
+                    <div className="article_card__thumb-overlay">
+                      <Link href="#">
+                        <Button variant="outline">shop now</Button>
+                      </Link>
+                    </div>
+                    <div className="__thumb-bg-color"></div>
+                    <div className="__thumb-bg-image"></div>
+                  </div>
+                  <div className="article_card__footer">
+                    <div className="__footer-header">
+                      <h3>Wicked: The Soundtrack (Picture Disc Vinyl)</h3>
+                      <h4>$51.00</h4>
+                    </div>
+                  </div>
+                </figure>
+              </article>
+            </div>
+          </div>
 
           <div className="mx-auto w-[95%] my-10 flex justify-end ">
             <Link href="/artists" className="mb-[50px] w-fit">
@@ -36,5 +140,6 @@ const ShopSection = () => {
 
 export default ShopSection;
 
-
-{/* <HiOutlineArrowNarrowRight /> */}
+{
+  /* <HiOutlineArrowNarrowRight /> */
+}
