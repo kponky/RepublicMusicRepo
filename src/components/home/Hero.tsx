3010538be3e29ce7f20d5a9b1e77ee291fc5c930
John Mason <87030324+simkidd@users.noreply.github.com>
2024-11-02 17:10:39 +0100

    adjust swiper

--- src/components/home/Hero.tsx
+++ src/components/home/Hero.tsx
@@ -14,19 +14,20 @@ const Hero = () => {
       <Swiper
         spaceBetween={30}
         centeredSlides={true}
-        direction={"vertical"}
-        autoplay={{
-          delay: 5000,
-          disableOnInteraction: false,
-        }}
+        direction="vertical"
+        // autoplay={{
+        //   delay: 5000,
+        //   disableOnInteraction: false,
+        // }}
         loop
-        modules={[Autoplay, Pagination]}
+        modules={[Pagination]}
         pagination={{ clickable: true }}
+        className="w-full h-full"
       >
         {banners.map((item, i) => {
           return (
             <SwiperSlide key={i}>
-              <div className="relative">
+              <div className="relative w-full">
                 <div className="banner_bg">
                   <div
                     className="single__banner"
@@ -34,20 +35,22 @@ const Hero = () => {
                       backgroundImage: `url(${item.imageUrl})`,
                     }}
                   ></div>
+                  {/* <div
+                    className="single__banner"
+                    style={{
+                      backgroundImage: `url(${item.imageUrl})`,
+                    }}
+                  ></div> */}
                 </div>
                 <div className="banner_fg">
-                  <div className="max-w-[850px] text-right w-full relative z-[1] text-white top-0">
+                  <div className="max-w-[850px] ml-auto text-right w-full relative z-[1] text-white top-0">
                     <div>
-                      <div className="">
-                        <h3 className="text-[145px] lowercase leading-[0.9] -tracking-wider">
-                          {item.title}
-                        </h3>
-                      </div>
-                      <div>
-                        <p className="w-[80%] text-base ml-auto leading-6">
-                          {item.subtitle}
-                        </p>
-                      </div>
+                      <h3 className="text-[145px] lowercase leading-[0.9] -tracking-wider">
+                        {item.title}
+                      </h3>
+                      <p className="w-[80%] text-base ml-auto leading-6">
+                        {item.subtitle}
+                      </p>
                       <div className="flex">
                         <Link href={item.link} className="ml-auto mt-[30px]">
                           <Button variant="outline">listen now</Button>
