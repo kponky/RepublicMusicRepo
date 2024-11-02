8a511a8852e42925d9cd912019f5aa4d5f38e0d8
John Mason <johnmsn22@gmail.com>
2024-11-02 02:14:35 +0100

    added artist data

--- src/components/home/ArtistCard.tsx
+++ src/components/home/ArtistCard.tsx
@@ -1,16 +1,39 @@
+import { Artist } from "@/data/artists";
+import Link from "next/link";
 import React from "react";
+import { FaTiktok } from "react-icons/fa6";
 
-const ArtistCard = () => {
+const ArtistCard = ({ artist }: { artist: Artist }) => {
   return (
     <div className="artist-card w-1/4 relative group">
       {/* bg image */}
-      <div className="artist-image"></div>
+      <div
+        className="artist-image"
+        style={{ backgroundImage: `url(${artist.imageUrl})` }}
+      ></div>
       {/* overlay */}
-      <div className="artist-overlay flex justify-between w-full h-full z-[2] p-5 absolute top-0 left-0 opacity-0 group-hover:opacity-100 bg-[#aba48b]">
-        <div> view website</div>
-        <div>social icons</div>
+      <div className="artist-overlay flex justify-between w-full h-full z-[2] p-5 absolute top-0 left-0 opacity-0 group-hover:opacity-100 bg-[#aba48b56]">
+        <div>
+          <Link href={artist.website} className="text-primary text-base">view website</Link>
+        </div>
+        <div>
+          <ul className="flex items-center gap-2">
+            {artist.socialLinks &&
+              Object.entries(artist.socialLinks).map(
+                ([key, { link, icon: Icon }]) => (
+                  <li>
+                    <Link href={link} className="text-primary text-base">
+                      <Icon size={20} />
+                    </Link>
+                  </li>
+                )
+              )}
+          </ul>
+        </div>
       </div>
-      <h3 className="text-[40px] absolute bottom-5 left-5 leading-none text-white artist-name">Taylor Swift</h3>
+      <h3 className="text-[40px] absolute bottom-5 left-5 leading-none text-white artist-name">
+        {artist.name}
+      </h3>
     </div>
   );
 };
--- src/components/home/ArtistSection.tsx
+++ src/components/home/ArtistSection.tsx
@@ -2,18 +2,19 @@ import React from "react";
 import ArtistCard from "./ArtistCard";
 import Link from "next/link";
 import Button from "../shared/Button";
+import { artists } from "@/data/artists";
 
 const ArtistSection = () => {
   return (
     <section className="mt-20">
-      <div className="z-[1] translate-y-10 relative">
+      <div className="z-[1] relative translate-y-7">
         <h2 className="text-center text-[160px] lowercase font-bold text-white -tracking-wider leading-none">
           artists
         </h2>
       </div>
-      <div className="w-full flex mx-auto flex-row flex-wrap">
-        {[...Array(20)].map((_, i) => (
-          <ArtistCard key={i} />
+      <div className="w-full flex mx-auto flex-row flex-wrap relative">
+        {artists.map((artist, i) => (
+          <ArtistCard key={i} artist={artist} />
         ))}
       </div>
       <div className="mx-auto w-[95%] my-10 flex justify-center ">
