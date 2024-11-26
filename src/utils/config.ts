ea01234c283dbc8b82a743ccd491b2c723754a24
John Mason <johnmsn22@gmail.com>
2024-11-26 14:24:03 +0100

    config file

---
+++ src/utils/config.ts
@@ -0,0 +1,10 @@
+export const config = {
+  SITE_NAME: process.env.SITE_NAME || "Republic Records",
+  SITE_DESCRIPTION:
+    process.env.SITE_DESCRIPTION ||
+    "Republic Records is a music streaming platform dedicated to sharing and discovering new artists and songs.",
+  SITE_URL: process.env.SITE_URL || "https://republicrecords.com",
+  SITE_OG_IMAGE: "/og-image.jpg",
+  SITE_TWITTER_HANDLE: "@republicrecords",
+  SITE_EMAIL: "info@republicrecords.com",
+};
