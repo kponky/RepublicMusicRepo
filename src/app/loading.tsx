7202770b4617e61751a45891a5382c06701aa580
John Mason <johnmsn22@gmail.com>
2025-01-31 15:08:34 +0100

    fix loader position

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,12 +1,10 @@
 export default function Loading() {
   return (
     <div className="w-full h-dvh bg-black relative">
-      <div className="fixed inset-0 flex items-center justify-center">
-        <div className="relative h-32 w-32">
-          <div className="circle one"></div>
-          <div className="circle two"></div>
-          <div className="circle three"></div>
-        </div>
+      <div className="absolute top-1/2 left-1/2 m-[-90px_0_0_-90px]">
+        <div className="circle one"></div>
+        <div className="circle two"></div>
+        <div className="circle three"></div>
       </div>
     </div>
   );
